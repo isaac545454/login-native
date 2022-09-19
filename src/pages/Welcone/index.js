d13be7fa4d220492784b2f618370993ce065817e
isaac gomes <93602614+isaac545454@users.noreply.github.com>
2022-09-19 09:33:27 -0300

    inicio

---
+++ src/pages/Welcone/index.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View>
+      <Text>pagina de entrada</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
